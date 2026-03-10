be849ca39676730782870c250ed97c68b331ea32
Derek-grc <derek.rios197@gmail.com>
2026-03-10 18:00:20 +0100

    Added caselles al DOM amb bucles fila/columna

--- script.js
+++ script.js
@@ -9,4 +9,16 @@ function init() {
   root.style.setProperty("--mida", midaCasella + "px")
   root.style.setProperty("--files", numFiles)
   root.style.setProperty("--columnes", numColumnes)
+
+    const refTauler = document.getElementById("tauler")
+
+  for (let fila = 0; fila < numFiles; fila++) {
+    for (let columna = 0; columna < numColumnes; columna++) {
+      const casella = document.createElement("div")
+      casella.classList.add("casella")
+      casella.style.left = `${columna * midaCasella}px`
+      casella.style.top = `${fila * midaCasella}px`
+      refTauler.appendChild(casella)
+    }
+  }
 }
